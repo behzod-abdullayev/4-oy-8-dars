90eec2d14a128f7996d2d247dd3047c3dfb6a871
behzod07-abdullayev <bekzod2366@gmail.com>
2025-12-01 21:13:31 +0500

    4-oy-8-dars

--- routes/data.routes.js
+++ routes/data.routes.js
@@ -6,6 +6,16 @@ const {
   updateData,
   deleteData,
   updateadmin,
+  getAllCar,
+  getOneCar,
+  addCar,
+  updateCar,
+  deleteCar,
+  getAllLaptop,
+  getOneLaptop,
+  addLaptop,
+  updateLaptop,
+  deleteLaptop,
 } = require("../controller/data.controller");
 const {
   authorization,
@@ -23,4 +33,19 @@ dataRouter.delete("/delete_data/:id", authorization, deleteData);
 // superadmin uchun
 dataRouter.put("/update_user/:id", authorization2, updateadmin);
 
+//                                                             qoshimcha
+
+//car
+dataRouter.get("/get_all_car", getAllCar);
+dataRouter.get("/get_one_car", getOneCar);
+dataRouter.post("/add_car", addCar);
+dataRouter.put("/update_car", updateCar);
+dataRouter.delete("/delete_car", deleteCar);
+
+//laptop
+dataRouter.get("/get_all_laptop", getAllLaptop);
+dataRouter.get("/get_one_laptop", getOneLaptop);
+dataRouter.post("/add_laptop", addLaptop);
+dataRouter.put("/update_laptop", updateLaptop);
+dataRouter.delete("/delete_laptop", deleteLaptop);
 module.exports = dataRouter;
